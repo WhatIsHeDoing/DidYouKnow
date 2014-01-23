394f1a7cb1f17fff9cff99e6a93f382b1fe3481a
Darren Hickling <darren@hickling.is>
2014-01-23 23:18:09 +0000

    C#: Kick-Off
    Adding a Visual Studio solution that contains a unit testing project
    with the first example: chaining the null-coalescing operator (long
    name, brief test)

---
+++ csharp/csharp/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csharp
+{
+    /// <summary>
+    /// Run me with Test => Run => All Tests
+    /// </summary>
+    [TestClass]
+    public class Main
+    {
+        /// <summary>
+        /// Demonstrates chaining the null-coalescing operator
+        /// </summary>
+        /// <remarks>http://msdn.microsoft.com/en-us/library/ms173224.aspx</remarks>
+        [TestMethod]
+        public void TestChainingNullCoalescingOperator()
+        {
+            int? firstNull = null;
+            int? secondNull = null;
+            Assert.AreEqual(firstNull ?? secondNull ?? 123, 123);
+        }
+    }
+}
